cc35e3b4f16ad5c7ed8276d8fd20f61732b7fc0c
Justin Smallwood <justin.d.smallwood@gmail.com>
2018-01-31 14:25:25 +1100

    Fixed Excel and Word document timestamps.
    Various changes to make merge work.
    
    Added "Download Rolls" button to timetable page.

---
+++ attendance/config.py
@@ -0,0 +1,39 @@
+appcfg = {
+    # Path to upload files directory
+    "upload": '/path/to/uploads/directory',
+    # Database string
+    "dbstring": 'sqlite:////path/to/database.db',
+    # Secret key for password hashing
+    "secretkey": 'internationalhouseattendanceprogram',
+    # Administrator password
+    "adminpassword": 'password',
+    # Logging file
+    "log": 'C:/Users/justi/log.log',
+	"startyear" : '2018',
+    "startstudyperiod" : "Semester 1",
+    "rooms" : [
+        ['GHB1', True],
+        ['GHB2', True],
+        ['GHB3', True],
+        ['GHB4', False],
+        ['GHB5', False],
+        ['GHB6', False],
+        ['GHB7', False],
+        ['Peter Waylen', False],
+        ['Ronald Cowan', True],
+        ['Frank Larkins', False],
+        ['Mavis Jackson', True],
+        ['Library Project Room', False]
+    ],
+    "timeslots": [
+        ['Monday 19:30', True],
+        ['Monday 20:30', True],
+        ['Monday 21:30', False],
+        ['Tuesday 19:30', True],
+        ['Tuesday 20:30', True],
+        ['Tuesday 21:30', False],
+        ['Wednesday 19:30', True],
+        ['Wednesday 20:30',True],
+        ['Wednesday 21:30', False]
+    ]
+}
